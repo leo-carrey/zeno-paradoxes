fbf1c7ba3c08aac9a949c4da50ef5162d616bc33
leo-carrey <leo.carrey@laplateforme.io>
2023-09-08 10:45:06 +0200

    [FIX] class zeno

---
+++ .vscode/methods/paradox-of-dichotomy.py
@@ -0,0 +1,10 @@
+class Zeno:
+    def __init__(self, distance) -> int:
+        self.distance = distance
+
+    def rock(self):
+        for i in range(10):
+            self.distance /= 2
+            print(f'the stone is {self.distance} away from the tree')
+
+Zeno(20).rock() 
